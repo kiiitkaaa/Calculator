e7757aa2d1ffaba7f6248b8db8c19c82f3d6e74f
kiiitkaaa <nikitadeshko5@gmail.com>
2025-03-19 15:36:54 +0300

    testFunctions

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class CalculatorClass
+    {
+        public string Expression { get; set; }
+
+        public string GetAnswer()
+        {
+
+            string primer = "1+2";
+            string first = new DataTable().Compute(primer, null).ToString();
+            return "answer";
+        }
+    }
+}
--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -16,8 +16,68 @@ namespace Calculator;
 /// </summary>
 public partial class MainWindow : Window
 {
+    CalculatorClass calculatorClass = new CalculatorClass();
+
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        switch (e.Key)
+        {
+            case Key.D0:
+                calculatorClass.Expression += "0";
+                break;
+            case Key.D1:
+                calculatorClass.Expression += "1";
+                break;
+            case Key.D2:
+                calculatorClass.Expression += "2";
+                break;
+            case Key.D3:
+                calculatorClass.Expression += "3";
+                break;
+            case Key.D4:
+                calculatorClass.Expression += "4";
+                break;
+            case Key.D5:
+                calculatorClass.Expression += "5";
+                break;
+            case Key.D6:
+                calculatorClass.Expression += "6";
+                break;
+            case Key.D7:
+                calculatorClass.Expression += "7";
+                break;
+            case Key.D8:
+                calculatorClass.Expression += "8";
+                break;
+            case Key.D9:
+                calculatorClass.Expression += "9";
+                break;
+            case Key.Add: // +
+                break;
+            case Key.Subtract: // -
+                break;
+            case Key.Multiply: // *
+                break;
+            case Key.Divide: // /
+                break;
+            case Key.Enter: // =
+                TextBlockAnswer.Text += "=" + calculatorClass.GetAnswer();
+                break;
+            case Key.Back: // Backspace
+                calculatorClass.Expression.Remove(calculatorClass.Expression.Length - 1);
+                break;
+            case Key.Delete:
+                break;
+            case Key.OemPeriod: // .
+                break;
+            default:
+                break;
+        }
+        TextBlockAnswer.Text = calculatorClass.Expression;
     }
 }
