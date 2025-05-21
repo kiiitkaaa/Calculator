af90e8ca88f7425c4c4c932781b6eaac24078b44
kiiitkaaa <168138033+kiiitkaaa@users.noreply.github.com>
2025-05-21 16:39:21 +0300

    Add files via upload

---
+++ Calculator/Style/ElementStyle.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculator.Config
+{
+    public class ElementStyle
+    {
+        public string Background { get; set; } = string.Empty;
+        public string Foreground { get; set; } = string.Empty;
+        public double FontSize { get; set; }
+    }
+}
---
+++ Calculator/Style/ThemeConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Calculator.Config
+{
+    public class ThemeConfig
+    {
+        public ElementStyle DigitButton { get; set; }
+        public ElementStyle OperationButton { get; set; }
+        public ElementStyle FunctionButton { get; set; }
+        public ElementStyle HistoryWindow { get; set; }
+        public ElementStyle HistoryControl { get; set; }
+    }
+}
